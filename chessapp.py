cd6011d4239337e12942ab134bb64e0943d8aa93
tinnit0 <leusinkthomas@gmail.com>
2023-12-20 11:24:20 +0100

    HELP!!!!

--- chessapp.py
+++ chessapp.py
@@ -9,7 +9,7 @@ import chess.engine
 import sys
 import threading
 import queue
-
+import pygame
 
 class chessapp:
     def __init__(self, root, ai):
@@ -23,12 +23,21 @@ class chessapp:
         self.teaching = False
         self.teaching_speed_limit = 5
         self.ai_instance = AI()
-
+        pygame.init()
+        pygame.mixer.init()
+        clock = pygame.time.Clock()
+        clock.tick(60)
+        
         self.setup_ui()
         self.update_score_display()
         self.stockfish_queue = queue.Queue()
 
         self.draw_board()
+        self.move_sound = pygame.mixer.Sound('ChessAiApp\\sounds\\move-self.mp3')
+        self.capture_sound = pygame.mixer.Sound('ChessAiApp\\sounds\\capture.mp3')
+        self.castling_sound = pygame.mixer.Sound('ChessAiApp\\sounds\\castle.mp3')
+        self.promoting_sound = pygame.mixer.Sound('ChessAiApp\\sounds\\promote.mp3')
+        self.checking_sound = pygame.mixer.Sound('ChessAiApp\\sounds\\move-check.mp3')
 
     def setup_ui(self):
         self.menu_frame = tk.Frame(self.root)
@@ -37,7 +46,7 @@ class chessapp:
         buttons = [
             ("Reset", self.reset_game),
             ("AI vs AI", self.ai_vs_ai),
-            ("Human vs AI", self.human_vs_ai),
+            ("Player vs AI", self.human_vs_ai),
             ("Teaching Mode", self.start_teaching_mode),
             ("AI vs Stockfish (1000)", self.start_ai_vs_stockfish),
         ]
@@ -58,26 +67,85 @@ class chessapp:
                 Image.open(image_path))
         return self.image_cache[image_name]
 
+    def animate_piece_movement(self, from_square, to_square):
+        from_row, from_col = 7 - (from_square // 8), from_square % 8
+        to_row, to_col = 7 - (to_square // 8), to_square % 8
+
+        piece = self.board.piece_at(from_square)
+        image = self.get_piece_image(piece)
+        label = tk.Label(self.root, image=image,
+                        bg=('brown' if (from_row + from_col) % 2 == 0 else 'white'))
+        label.image = image
+
+        label.grid(row=from_row, column=from_col)
+
+        def animate_frame(frame):
+            current_row = int((from_row * (1 - frame)) + (to_row * frame))
+            current_col = int((from_col * (1 - frame)) + (to_col * frame))
+
+            label.grid(row=current_row, column=current_col)
+
+            print("About to play move sound")
+            self.move_sound.play()
+            print("Move sound played")
+
+            for frame in range(1, 11):
+                self.root.after(frame * 100, lambda f=frame/10: animate_frame(f))
+
+
+    def batch_update_ai_moves(self, moves, teach_mode=False):
+        move_delay = self.teaching_speed_limit if self.teaching else self.ai_delay
+
+        def update_moves(moves):
+            for move in moves:
+                if move in self.board.legal_moves:
+                    self.board.push(move)
+                    print(f"Making AI move: {move.uci()}")
+
+                    if not teach_mode:
+                        self.update_square(move.from_square %
+                                        8, 7 - (move.from_square // 8))
+                        self.update_square(move.to_square %
+                                        8, 7 - (move.to_square // 8))
+
+                    if self.board.is_capture(move):
+                        self.capture_sound.play()
+                        print("Playing capture sound")
+
+            if self.board.turn == chess.BLACK:
+                ai_move = self.ai.choose_move(self.board)
+                self.board.push(ai_move)
+                self.update_square(ai_move.from_square %
+                                8, 7 - (ai_move.from_square // 8))
+                self.update_square(ai_move.to_square %
+                                8, 7 - (ai_move.to_square // 8))
+
+        for i, move in enumerate(moves):
+            self.root.after(i * move_delay, lambda m=move: update_moves([m]))
+
     def draw_board(self):
         for row in range(8):
             for col in range(8):
                 self.create_square(row, col)
 
     def create_square(self, row, col):
-        color = 'brown' if (row + col) % 2 == 0 else 'white'
-        frame = tk.Frame(self.root, width=60, height=60, bg=color)
-        frame.grid(row=row, column=col)
+        color = '#d9d9d9' if (row + col) % 2 == 0 else '#a9a9a9'
+        frame = tk.Frame(self.root, width=60, height=60,
+                        bg=color, borderwidth=1, relief="solid")
+        frame.grid(row=row, column=col, sticky="nsew")
+        frame.grid_propagate(False)
+
         frame.bind("<Button-1>", lambda event, r=row,
-                   c=col: self.on_square_click(r, c))
+                c=col: self.on_square_click(r, c))
 
         piece = self.board.piece_at(chess.square(col, 7 - row))
         if piece:
-            label = tk.Label(
-                frame, image=self.get_piece_image(piece), bg=color)
-            label.image = self.get_piece_image(piece)
+            image = self.get_piece_image(piece)
+            label = tk.Label(frame, image=image, bg=color)
+            label.image = image
             label.pack(fill=tk.BOTH, expand=True)
             label.bind("<Button-1>", lambda event, r=row,
-                       c=col: self.on_square_click(r, c))
+                    c=col: self.on_square_click(r, c))
         else:
             frame.config(bg=color)
 
@@ -114,18 +182,33 @@ class chessapp:
             self.selected_label.config(
                 bg=('brown' if (row + col) % 2 == 0 else 'white'))
             self.clear_highlights()
-            self.update_square(move.from_square %
-                               8, 7 - (move.from_square // 8))
+            self.update_square(move.from_square % 8, 7 - (move.from_square // 8))
             self.update_square(move.to_square % 8, 7 - (move.to_square // 8))
 
             if move.promotion:
-                self.update_square(move.to_square %
-                                   8, 7 - (move.to_square // 8))
+                self.update_square(move.to_square % 8, 7 - (move.to_square // 8))
 
             if move in [m for m in self.board.legal_moves if m.uci().endswith('e.p.')]:
                 ep_rank = 5 if self.board.turn == chess.WHITE else 2
                 ep_square = chess.square(move.to_square % 8, ep_rank)
                 self.update_square(ep_square % 8, 7 - (ep_square // 8))
+
+            print("About to play move sound")
+            self.move_sound.play()
+            print("Move sound played")
+
+            if not self.board.is_game_over() and self.board.turn == chess.BLACK:
+                ai_move = self.ai.make_ai_move(self.board)
+                self.board.push(ai_move)
+                self.update_square(ai_move.from_square %
+                                8, 7 - (ai_move.from_square // 8))
+                self.update_square(ai_move.to_square %
+                                8, 7 - (ai_move.to_square // 8))
+
+        else:
+            self.selected_label.config(
+                bg=('brown' if (row + col) % 2 == 0 else 'white'))
+            self.selected_piece = None
 
     def on_square_click(self, row, col):
         position = chess.square(col, 7 - row)
